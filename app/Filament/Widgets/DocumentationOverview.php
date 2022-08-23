17b78b10e9103216ae770f338e07c8bc7c807a53
desplega <desplega@desplega.com>
2022-08-23 12:30:02 +0200

    Info widget

---
+++ app/Filament/Widgets/DocumentationOverview.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class DocumentationOverview extends Widget
+{
+    protected static ?int $sort = -2;
+
+    protected static string $view = 'filament.widgets.documentation-overview';
+}
--- config/filament.php
+++ config/filament.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Filament\Resources\MarketResource\Widgets\MarketStatsOverview;
+use App\Filament\Widgets\DocumentationOverview;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Http\Middleware\MirrorConfigToSubpackages;
@@ -141,7 +142,8 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
+            //Widgets\FilamentInfoWidget::class,
+            DocumentationOverview::class,
             MarketStatsOverview::class,
         ],
     ],
